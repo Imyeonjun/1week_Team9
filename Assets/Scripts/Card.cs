af4270c2b85c6c75b48f849405c97a739a84be68
Imyeonjun <imyeonjun0602@gmail.com>
2025-04-09 16:07:36 +0900

    Add card.cs / Change Assets

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -24,11 +24,49 @@ public class Card : MonoBehaviour
     {
         
     }
+
+    //파일명을 변경하지 않고 이미지를 불러오는 것을 코드로 짰습니다.
     public void Setting(int number)
     {
         idx = number;
-        frontImage.sprite = Resources.Load<Sprite>($"team{idx}");
+
+        //sprite 초기화
+        Sprite sprite = null;
+
+        if (idx == 0 || idx == 1)
+        {
+            sprite = Resources.Load<Sprite>($"Garam{idx}");
+        }
+        else if (idx == 2 || idx == 3)
+        {
+            //idx를 2로 나눈 나머지 값을 변수로 지정.
+            //idx 값에 따라 0 또는 1로 할당하여 이미지를 sprtie에 입력.
+            int spriteNum = idx % 2;
+            sprite = Resources.Load<Sprite>($"Jongmin{spriteNum}");
+        }
+        else if (idx == 4 || idx == 5)
+        {
+            int spriteNum = idx % 2;
+            sprite = Resources.Load<Sprite>($"Seongdeuk{spriteNum}");
+        }
+        else if (idx == 6 || idx == 7)
+        {
+            int spriteNum = idx % 2;
+            sprite = Resources.Load<Sprite>($"Sanghun{spriteNum}");
+        }
+        else if (idx == 8 || idx == 9)
+        {
+            int spriteNum = idx % 2;
+            sprite = Resources.Load<Sprite>($"Yeonjun{spriteNum}");
+        }
+
+        //유니티 내 frontImage에 sprite에 담은 이미지 할당.
+        if (sprite != null)
+        {
+            frontImage.sprite = sprite;
+        }
     }
+
     public void OpenCard()
     {
         anim.SetBool("isOpen", true);
