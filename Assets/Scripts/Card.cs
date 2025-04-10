44770023436bb3a26b3bcac81718339c1621a9c1
Imyeonjun <imyeonjun0602@gmail.com>
2025-04-10 16:38:00 +0900

    Backup

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -6,7 +6,6 @@ public class Card : MonoBehaviour
 {
 
     public int idx = 0;
-    public int spriteNum;
 
     public GameObject front;
     public GameObject back;
@@ -25,41 +24,11 @@ public class Card : MonoBehaviour
     {
         
     }
-
-    //파일명 유지 (파일명이 {name}{0,1}이라는 조건 하에)
-    public class Title : MonoBehaviour
+    public void Setting(int number)
     {
-        private int idx;
-        private int spriteNum;
-
-        public void Setting(int number)
-        {
-            idx = number;
-
-            string[] names = { "Garam", "Garam", "Jongmin", "Jongmin", "Seongdeuk", "Seongdeuk", "Sanghun", "Sanghun", "Yeonjun", "Yeonjun" };
-
-            if (idx >= 0 && idx < names.Length)
-            {
-                spriteNum = (idx <= 1) ? idx : idx % 2;
-                string name = names[idx];
-                Sprite sprite = Resources.Load<Sprite>($"{name}{spriteNum}");
-
-                if (sprite != null)
-                {
-                    SpriteRenderer frontImage = null;
-                    frontImage.sprite = sprite;
-                }
-            }
-        }
-
-        // StartSceneBoard에서 호출할 호환용 함수
-        public void SettingT(int imageNum, string text, int unused)
-        {
-            Setting(imageNum);
-            // text 파라미터를 활용하고 싶으면 여기에 텍스트 반영 로직 추가
-        }
+        idx = number;
+        frontImage.sprite = Resources.Load<Sprite>($"team{idx}");
     }
-
     public void OpenCard()
     {
         anim.SetBool("isOpen", true);
