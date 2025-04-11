47ebd14793cac66b08a59989b00cc3787c3489a4
Imyeonjun <imyeonjun0602@gmail.com>
2025-04-11 10:07:04 +0900

    Update Title.cs
    주석 수정

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -6,7 +6,7 @@ public class Title : MonoBehaviour
     public SpriteRenderer title;
     public Text titleText;
 
-    // 함수에 지정된 매개 변수 int tt와 string label은 호출하는 쪽에서 지정할 것.
+    // 함수에 지정된 매개 변수는 호출하는 쪽에서 지정할 것.
     // 함수 안의 로직은 이 매개 변수를 이용할 수 있어야 할 것.
     public void SettingT(string name, int tt, string label)
     {
